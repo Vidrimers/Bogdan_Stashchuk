2e9f2feb649fe224c70815c97fdf7ee8059f9767
Vidrimers <vidrimers2@gmail.com>
2022-05-11 18:43:59 +0300

    vars

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Богдан Стащук JS 11 часов')
+
+
+let myName
+
+console.log(myName)
+
+myName = 'Yaro' // заново лет уже не надо писать, потому что переменная уже была определена
+
+console.log(myName)
